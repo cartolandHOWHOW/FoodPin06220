d13828c88dea8bd8987afd032ede0567e1beda49
cartolandHOWHOW <hehowere3@gmail.com>
2025-06-20 13:08:17 +0800

    init project

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,30 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by max on 2025/6/16.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+    init() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor:UIColor(red:218 , green:96, blue: 51), .font: UIFont(name:"ArialRoundedMTBold", size: 35)!]
+        navBarAppearance.titleTextAttributes = [.foregroundColor:UIColor(red:218 , green:96, blue: 51), .font: UIFont(name:"ArialRoundedMTBold", size: 20)!]
+        navBarAppearance.backgroundColor = .clear
+        navBarAppearance.backgroundEffect = .none
+        navBarAppearance.shadowColor = .clear
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        UINavigationBar.appearance().compactAppearance = navBarAppearance
+    }
+    
+}
+
